e4db6f30efa02915dfd54e133c5a862397ddfb5a
stephaniehourcade <stephaniehourcade@gmail.com>
2018-03-12 01:55:03 -0300

    firstcommit

---
+++ src/com/Herencia/Stephanie/Estudiante.java
@@ -0,0 +1,25 @@
+package com.Herencia.Stephanie;
+
+public class Estudiante extends Persona {
+	
+	private int codigoEstudiante;
+	private float notaFinal;
+	
+	//Constructor
+	
+	public Estudiante(String nombre, String apellido, int edad, int codigoEstudiante, float notaFinal) {
+		super(nombre, apellido, edad);
+		this.codigoEstudiante = codigoEstudiante;
+		this.notaFinal = notaFinal;
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("Nombre: " + getNombre() +
+"\nApellido: " + getApellido() +
+"\nEdad: " + getEdad() + 
+"\nCodigo de Estudiante: " + codigoEstudiante +
+"\nNota Final: " + notaFinal);
+	}
+	
+
+}
---
+++ src/com/Herencia/Stephanie/Principal.java
@@ -0,0 +1,13 @@
+package com.Herencia.Stephanie;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Estudiante estudiante = new Estudiante("Mabel", "Rodriguez"
+		, 25, 1564, 9);
+		
+		estudiante.mostrarDatos();
+	}
+
+}
